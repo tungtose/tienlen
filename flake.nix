e95a6fd2c2fc4eb61ace5bd8a4d7c635bc23b174
spruce <tungtose@gmail.com>
2023-06-13 01:25:19 +0700

    update: welcome nixos

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "rust shell dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            exa
+            fd
+            cargo-watch
+            wasm-pack
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+            alias find="cargo build"
+            alias run="cargo run --release --target wasm32-unknown-unknown"
+          '';
+        };
+      }
+    );
+}
