738815fdd70039a1041b9bdd818da197832c67a4
spruce <tungtose@gmail.com>
2023-07-18 02:58:35 +0700

    feat: server dockerization

--- server/src/systems/init.rs
+++ server/src/systems/init.rs
@@ -14,11 +14,11 @@ pub fn init(mut commands: Commands, mut server: Server) {
 
     // Naia Server initialization
     let server_addresses = webrtc::ServerAddrs::new(
-        "127.0.0.1:14191"
+        "0.0.0.0:14191"
             .parse()
             .expect("could not parse Signaling address/port"),
         // IP Address to listen on for UDP WebRTC data channels
-        "127.0.0.1:14192"
+        "0.0.0.0:14192"
             .parse()
             .expect("could not parse WebRTC data address/port"),
         // The public WebRTC IP address to advertise
