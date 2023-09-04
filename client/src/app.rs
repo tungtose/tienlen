6422aad3cb4007d0447eccc19f1cf3a70c216159
spruce <tungtose@gmail.com>
2023-09-04 07:32:49 +0700

    update: web styling

--- client/src/app.rs
+++ client/src/app.rs
@@ -27,8 +27,9 @@ pub fn run() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "thirteen".into(),
+                canvas: Some("#thirteen".into()),
                 resolution: (800., 500.).into(),
-                fit_canvas_to_parent: true,
+                fit_canvas_to_parent: false,
                 prevent_default_event_handling: false,
                 ..default()
             }),
